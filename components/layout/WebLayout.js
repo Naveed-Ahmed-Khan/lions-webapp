30cd37f6d7a46432c526bf4b1b286ff5923fd5a7
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-10-21 10:28:45 -0700

    message of unverification at top

--- components/layout/WebLayout.js
+++ components/layout/WebLayout.js
@@ -28,6 +28,20 @@ export default function WebLayout({ children }) {
       />
       <Navbar open={open} setOpen={setOpen} setShowBackdrop={setShowBackdrop} />
 
+      {currentUser?.profileStatus && !currentUser?.isVerified && (
+        <div className="px-5 xl:px-0">
+          <p
+            // onClick={() => {
+            //   setError("");
+            // }}
+            className="max-w-screen-xl mx-auto cursor-pointer mt-2 text-center font-archivo text-red-500 px-6 py-3 border border-red-500 rounded-lg"
+          >
+            An unverified tutor cannot apply on Jobs. Pay your verification fee
+            of Rs 1500 to 03328200082 Jazz cash to get verified.
+          </p>
+        </div>
+      )}
+
       <div className="z-0">{children}</div>
       <Footer />
     </div>
