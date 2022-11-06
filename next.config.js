dec4f1c39f96b34bda5ddacf7a48fe7d60e3add8
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-11-06 04:42:06 -0800

    ssg timeout increased

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  staticPageGenerationTimeout: 180,
+  staticPageGenerationTimeout: 1800,
 };
 
 module.exports = {
